22e66f280d0229ef0e96eccc91dd45c7e3d921ae
Ishan <ishan797909@gmail.com>
2026-08-08 17:52:43 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        rob1,rob2 = 0,0
+
+        #[rob1,rob2,n,n+1...]
+
+        for i in nums:
+            temp = max(i + rob1,rob2)
+            rob1 = rob2
+            rob2 = temp
+
+        return rob2
+        
